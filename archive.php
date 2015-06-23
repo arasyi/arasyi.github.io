8885fbcefc9443e66a8daff1e737d0b48a5b1366
arasyi <arasyi@users.noreply.github.com>
2015-06-23 15:05:30 +0700

    delete all

--- archive.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$str = file_get_contents('archive/2015.html');
-for ($i = 2016; $i <= 2020; $i++) {
-	$str2 = str_replace('2015', $i, $str);
-	file_put_contents("archive/$i.html", $str2);
-}
--- new.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-thor jekyll:new %*
